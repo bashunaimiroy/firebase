6f443eebcd65c3a3c64e0db1f14b91f4c0e57150
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2017-10-30 22:13:20 -0400

    now with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from "firebase";
 import './App.css';
+
+var config = {
+  apiKey: "AIzaSyC6Mdq8bYCdiZN7J1Kpuovj_FSASKbBMJg",
+  authDomain: "my-beginner-project.firebaseapp.com",
+  databaseURL: "https://my-beginner-project.firebaseio.com",
+  projectId: "my-beginner-project",
+  storageBucket: "my-beginner-project.appspot.com",
+  messagingSenderId: "321603513191"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div></div>
+         );
   }
 }
 
